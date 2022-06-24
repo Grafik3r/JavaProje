6c8762cb776b03e00a9f64e9bd5e037df2dc10a7
star <star@DESKTOP-O13OKIM>
2022-06-24 19:12:28 +0300

    24

--- Intro3/src/Main.java
+++ Intro3/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Merhaba Dünya");
 		System.out.println("Hello World!!!");
-		
+		System.out.println("Hello World!!!");
 		// TODO Auto-generated method stub
 
 	}
